0aa5d7dfe0c19202b607ffe93f14aa80483c8c01
Thinegan Ratnam <thinegan@gmail.com>
2016-12-14 14:24:52 +0800

    Add Confirm Password Field In WooCommerce Registration Page

--- staging.kasturicookies.com/wp-content/themes/bakery/functions.php
+++ staging.kasturicookies.com/wp-content/themes/bakery/functions.php
@@ -182,3 +182,28 @@ if ( $wpdb->get_var('SELECT count(*) FROM `' . $wpdb->prefix . 'datalist` WHERE 
 		add_action('admin_notices', 'vu_setup_menus_notice');
 	}
 ?>
+<?php
+// Add the code below to your theme's functions.php file to add a confirm password field on the register form under My Accounts.
+add_filter('woocommerce_registration_errors', 'registration_errors_validation', 10,3);
+function registration_errors_validation($reg_errors, $sanitized_user_login, $user_email) {
+	global $woocommerce;
+	extract( $_POST );
+	if ( strcmp( $password, $password2 ) !== 0 ) {
+		return new WP_Error( 'registration-error', __( 'Passwords do not match.', 'woocommerce' ) );
+	}
+	return $reg_errors;
+}
+add_action( 'woocommerce_register_form', 'wc_register_form_password_repeat' );
+function wc_register_form_password_repeat() {
+?>
+	<p class="form-row form-row-wide">
+		<label for="reg_password2"><?php _e( 'Retype Password', 'woocommerce' ); ?> <span class="required">*</span></label>
+		<input type="password" class="woocommerce-Input woocommerce-Input--text input-text form-control" name="password2" id="reg_password2" value="<?php if ( ! empty( $_POST['password2'] ) ) echo esc_attr( $_POST['password2'] ); ?>" />
+	</p>
+
+<p class="form-row form-row-wide">
+		<label for="reg_password2"><?php _e( 'By signing up, I confirm that i agree to <a target="_blank" href="https://staging.kasturicookies.com/privacy-policy/">Privacy Policy</a> and <a target="_blank" href="https://staging.kasturicookies.com/terms-condition/">Term & Condition</a>', 'woocommerce' ); ?></label>
+</p>
+	<?php
+}
+?>
